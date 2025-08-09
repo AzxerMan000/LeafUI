40198195953ba3e8e570745d755c4827822af6a7
Azxerman <bangjoof12@gmail.com>
2025-08-09 16:15:02 +0700

    Create themes.lua

---
+++ Main/themes.lua
@@ -0,0 +1,16 @@
+local Themes = {
+    Default = {
+        Background = Color3.fromRGB(30, 30, 30),
+        TabBackground = Color3.fromRGB(40, 40, 40),
+        Accent = Color3.fromRGB(0, 170, 127),
+        Text = Color3.fromRGB(255, 255, 255)
+    },
+    Light = {
+        Background = Color3.fromRGB(240, 240, 240),
+        TabBackground = Color3.fromRGB(220, 220, 220),
+        Accent = Color3.fromRGB(0, 120, 255),
+        Text = Color3.fromRGB(0, 0, 0)
+    }
+}
+
+return Themes
