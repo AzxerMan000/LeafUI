9413e86cebd69e599ac3620775a711da26f5fca7
Azxerman <bangjoof12@gmail.com>
2025-08-09 16:14:01 +0700

    Create init.lua

---
+++ Main/init.lua
@@ -0,0 +1,23 @@
+local Leaf = {}
+
+Leaf.Themes = loadstring(game:HttpGet(""))()
+Leaf.Window = loadstring(game:HttpGet(""))()
+Leaf.Tab = loadstring(game:HttpGet(""))()
+Leaf.Utils = loadstring(game:HttpGet(""))()
+
+-- Constructor
+function Leaf.new(title, themeName)
+    return Leaf.Window.new(title, themeName or "Default")
+end
+
+-- Theme setter
+function Leaf:SetTheme(gui, themeName)
+    local theme = Leaf.Themes[themeName]
+    if theme then
+        gui.Theme = theme
+        gui.MainFrame.BackgroundColor3 = gui.Theme.Background
+        gui.TabContainer.BackgroundColor3 = gui.Theme.TabBackground
+    end
+end
+
+return Leaf
